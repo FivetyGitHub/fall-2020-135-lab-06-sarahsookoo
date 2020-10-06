769a30ae2dc4d568bad4e50ee00e3e8ec685ac2c
sarahsookoo <sarah_sookoo@outlook.com>
2020-10-06 12:16:36 -0400

    removed comments

--- funcs.cpp
+++ funcs.cpp
@@ -7,7 +7,5 @@ void test_ascii(std::string str){
 
   for (int i=0; i<str.length(); i++){
      std::cout << str[i] << " " << (int)str[i] << std::endl;
-    // std::cout << 5 <<std::endl;
-    // return 0;
   }
 }
