e857e136e36e331a72c67781ab12b48a530b6522
sarahsookoo <sarah_sookoo@outlook.com>
2020-10-30 00:31:08 -0400

    always returns first string

--- decode.cpp
+++ decode.cpp
@@ -4,7 +4,7 @@
 #include "decrypt.h"
 #include "decode.h"
 
-/*int numChar(std::string str, char letter){
+int numChar(std::string str, char letter){
   int num = 0;
   for(int i=0; i<str.length(); i++){
     if (str[i] == letter || str[i] == char((int)letter - 32)){
@@ -12,29 +12,30 @@
     }
   }
   return num;
-  }*/
+  }
+
 std::string decode(std::string str){ //str is an encrypted string
   double freq[] = {8.2, 1.5, 2.8, 4.3, 13, 2.2, 2, 6.1, 7, .15, .77, 4, 2.4, 6.7,
 		  7.5, 1.9, .095, 6, 6.3, 9.1, 2.8, .98, 2.4, .15, 2, .074}; //freq of each letter as a percent
  std::string guesses [26]; //26 possible sentences
  double eachDis[26] = {0};
+ double num = 0;
+ std::vector<double> letters;
  
  for (int i=0; i<26; i++){
    guesses[i] = decryptCaesar(str, i); //decrypts the string creating 26 possible sentences
-   std::string& sen = guesses[i];
-   double count[26] = {0};
-   double numChar[26];
-   double dis[26];
-   
-   for (int i=0; i<sen.size(); i++){
-     if (islower(sen[i])){
-	 count[sen[i] - 'a']++;
-     }
-     else if (isupper(sen[i])){
-	 count[sen[i] - 'A']++;
-       }
+   std::string phr = guesses[i];
+   for (int i = 97; i<123; i++){
+     num = numChar(phr, char(i));
+     letters.push_back((num/str.length()) *100);
    }
+   double dis;
    for (int j=0; j<26; j++){
+     dis = dis + pow(freq[i] - letters[i], 2);
+   }
+   eachDis[i] = sqrt(dis);
+ }
+     /* for (int j=0; j<26; j++){
      numChar[j] = (count[j] / sen.length())*100;//makes it a percent
    }
    for (int k=0; k<26; k++){
@@ -43,11 +44,11 @@ std::string decode(std::string str){ //str is an encrypted string
    for (int l=0; l<26; l++){
      eachDis[l] += dis[l];
    }
- }
+   }*/
 
  int lowest = 0;
  for (int i=0; i<26; i++){
-   // std::cout << guesses[i] <<std::endl;
+   //   std::cout << guesses[i] <<std::endl;
    if (eachDis[i] < eachDis[lowest]){
      lowest = i;
    }
--- decode.h
+++ decode.h
@@ -1,3 +1,4 @@
 #pragma once
 
 std::string decode(std::string);
+int numChar(std::string str, char letter);
